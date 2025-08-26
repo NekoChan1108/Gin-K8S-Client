83a0562c372dfd012ba41243171980d54df528f6
wuziyang <uzg1108@outlook.com>
2025-08-26 22:37:27 +0800

    refactor(router): 更新 API路由引用
    - 移除 router 包中冗余的 api2 别名导入
    -将所有 api2 调用改为直接使用 api 包
    - 此修改统一了 API调用方式，简化了代码结构

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,7 +1,7 @@
 package router
 
 import (
-	api2 "Gin-K8S-Client/internal/api"
+	"Gin-K8S-Client/internal/api"
 	"Gin-K8S-Client/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -10,12 +10,12 @@ func InitRouter(r *gin.Engine) {
 	middleware.InitMiddleware(r)
 	mainGroup := r.Group("/api")
 	{
-		mainGroup.GET("/", api2.Hello)
+		mainGroup.GET("/", api.Hello)
 		k8sGroup := mainGroup.Group("/kubernetes")
 		{
-			k8sGroup.GET("/namespace", api2.GetNamespace)
-			k8sGroup.GET("/pod", api2.GetPod)
-			k8sGroup.GET("/exec/namespace/:namespace/podName/:podName/containerName/:containerName", api2.ExecPod)
+			k8sGroup.GET("/namespace", api.GetNamespace)
+			k8sGroup.GET("/pod", api.GetPod)
+			k8sGroup.GET("/exec/namespace/:namespace/podName/:podName/containerName/:containerName", api.ExecPod)
 		}
 	}
 }
